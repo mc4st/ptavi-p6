5d46c6ab230044191d2f65460dac846fe98cc848
marta castejon cachero <mcastejo@zeta40.aulas.gsyc.es>
2015-11-23 16:41:15 +0100

    Queda RTP

--- server.py
+++ server.py
@@ -5,27 +5,37 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
+        IP = self.client_address[0]
+        PORT =  self.client_address[1]
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
+            line_bytes = self.rfile.read()
+            line = line_bytes.decode('utf-8')
             if not line:
                 break
-
+            print("El cliente nos manda: \n" + line_bytes.decode('utf-8'))
+            (metodo, address, sip) = line.split()
+            if metodo not in ["INVITE","BYE","ACK"]:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed" + b"\r\n" + b"\r\n")
+            elif metodo == "INVITE":
+                self.wfile.write(b"SIP/2.0 100 Trying" + b"\r\n" + b"\r\n")
+                self.wfile.write(b"SIP/2.0 180 Ring" + b"\r\n" + b"\r\n")
+                self.wfile.write(b"SIP/2.0 200 OK" + b"\r\n" + b"\r\n")
+            elif metodo == "BYE":
+                self.wfile.write(b"SIP/2.0 200 OK" + b"\r\n" + b"\r\n")
+            else:
+                self.wfile.write(b"SIP/2.0 400 Bad Request" + b"\r\n" + b"\r\n")
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    PORT = int(sys.argv[2])
+    IP = sys.argv[1]
+    serv = socketserver.UDPServer((IP, PORT), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
